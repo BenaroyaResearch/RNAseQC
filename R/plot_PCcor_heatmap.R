f895f8fe3b4b7a791ccef04bb02dffba20abb6f3
mjdufort <matt.dufort@gmail.com>
2019-06-10 13:12:21 -0700

    Enable rotation of plot_PCcor_heatmap

--- R/plot_PCcor_heatmap.R
+++ R/plot_PCcor_heatmap.R
@@ -8,7 +8,8 @@
 #' @param plotdims a numeric vector, the size (in inches) of the plotting object (if outputting to a file)
 #' @param my_heatmap_cols a vector of color names, typically the result of a call to \code{colorRampPalette}. Default is a blue (low) to white (middle) to red (high) palette.
 #' @param center_colors_zero logical, whether to center the color scale on 0. Ignored if \code{my_heatmap_cols} is specified. Defaults to \code{TRUE}.
-#' @param row_dendro,col_dendro logical, whether to include the row and/or columns dendrogram(s). Both default to \code{FALSE}.
+#' @param PC_dendro,var_dendro logical, whether to include the row and/or columns dendrogram(s). Both default to \code{FALSE}.
+#' @param orientation character, either "horizontal" or "vertical", specifying the direction to plot the correlations. If "horizontal" (the default), PCs are rows and other variables are columns. If "vertical", PCs are columns and other variables are rows. Partial matches are allowed.
 #' @param remove_all_NA_cols logical, whether to remove variables where all the correlation values are NA. Defaults to \code{TRUE}.
 #' @param ... (optional) additional arguments passed to \code{ComplexHeatmap::Heatmap}.
 #' @export
@@ -16,7 +17,8 @@ plot_PCcor_heatmap <-
   function(PCcor_result, filename=NULL, plotdims=c(9,9),
            my_heatmap_cols, center_colors_zero=TRUE,
            key=TRUE, keysize=0.8, density.info="none", trace="none",
-           row_dendro=FALSE, col_dendro=FALSE,
+           PC_dendro=FALSE, var_dendro=FALSE,
+           orientation="horizontal",
            remove_all_NA_cols=TRUE,
            ...) {
     PCcor_result <- as.matrix(PCcor_result)
@@ -24,6 +26,9 @@ plot_PCcor_heatmap <-
     checkmate::assert(
       checkmate::check_numeric(PCcor_result)
     )
+    
+    orientation <-
+      match.arg(orientation, choices=c("horizontal", "vertical"))
     
     if (remove_all_NA_cols)
       PCcor_result <- miscHelpers::remove_all_NA_rowcols(PCcor_result, rows=FALSE)
@@ -47,6 +52,13 @@ plot_PCcor_heatmap <-
     if (!is.null(filename)) {
       pdf(file=filename, w=plotdims[1], h=plotdims[2])
       on.exit(dev.off()) # close plotting device on exit
+    }
+    
+    if (orientation == "horizontal") {
+      row_dendro <- PC_dendro; col_dendro <- var_dendro
+    } else if (orientation == "vertical") {
+      col_dendro <- PC_dendro; row_dendro <- var_dendro
+      PCcor_result <- t(PCcor_result)
     }
     
     # generate heatmap
